08562f549752c5eb6cecd27290030647d6ab7e60
andersonmontor <anderson.montor@hotmail.com>
2019-11-08 16:16:23 -0300

    +portability

--- stopwatch.py
+++ stopwatch.py
@@ -3,6 +3,7 @@ from time import sleep
 import os
 from datetime import datetime as dt
 from datetime import timedelta
+from sys import platform
 
 COMEUP_TIME = 2 * 3600. # 2 horas
 
@@ -13,7 +14,18 @@ def progress_bar(total_length, total, completed, text = ''):
 	return "%s[%s%s] %d%%" % (text, 'X' * n_bars, '-' * (bar_length - n_bars), int(percent*100))
 
 	# fazer as primeiras 2 horas em outra cor(come up)
-
+	
+def clear_screen():
+	if platform.startswith("win"):
+		os.system("cls")
+	elif platform.startswith("linux"):
+		os.system("clear")
+		
+def get_terminal_width():
+	if platform.startswith("linux"):
+		return int(os.popen('stty size', 'r').read().split()[1])
+	else:
+		return 80 # FIX ME
 
 if __name__ == '__main__':
     print "Horario atual: %d:%d:%d" % (dt.now().hour, dt.now().minute, dt.now().second)
@@ -32,7 +44,7 @@ if __name__ == '__main__':
     	total_duration = (later - start_time).seconds
 
     while True:
-        os.system("clear")
+        clear_screen()
         seconds_passed =  (dt.now() - start_time).seconds
         
         print "Duracao:  %02d:%02d:%02d" % (seconds_passed / 3600, (seconds_passed % 3600) / 60, (seconds_passed % 3600) % 60)
@@ -41,9 +53,7 @@ if __name__ == '__main__':
             print "Restante: %02d:%02d:%02d (%02d:%02d:%02d)" % (seconds_remaining / 3600, (seconds_remaining % 3600) / 60,
             													 (seconds_remaining % 3600) % 60, h2, m2, s2)
             print "Porcentagem comeup: %d%%" % ((COMEUP_TIME/total_duration)*100)
-
-            columns = int(os.popen('stty size', 'r').read().split()[1]) # largura de caracteres do terminal
-            print progress_bar(columns, total_duration, seconds_passed, 'Duracao: ')
+            print progress_bar(get_terminal_width(), total_duration, seconds_passed, 'Duracao: ')
         sleep(1)
 
         # TODO: quando o tempo acabar, avisar o user e mostrar quanto tempo ele ta de "overtime"
