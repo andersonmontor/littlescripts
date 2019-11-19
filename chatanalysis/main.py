3ea6d33391ab14785225c394fc0bb96553a6fa75
AndersonMontor <ander_1434@hotmail.com>
2019-11-19 12:53:20 -0200

    melhor modularizacao de algumas coisas(dar merge depois

--- chatanalysis/main.py
+++ chatanalysis/main.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
-import chatclasses as cc
+import chat as cc
+import plotter
 
 def main():
 	path = raw_input("Digite o path do arquivo de chat(normalizado): ")
 	
 	chat = cc.ChatLog(path)
+	plotter = plotter.ChatPlotter(chat)
+	plotter.plot_daily_count()
 
 if __name__ == '__main__':
 	main()
